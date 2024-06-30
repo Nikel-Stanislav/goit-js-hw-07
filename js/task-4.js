df25600a46303c65003d023ec2d4eb00c8f1e13e
Nikel Stanislav <162141430+Nikel-Stanislav@users.noreply.github.com>
2024-06-30 16:20:26 +0300

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -2,5 +2,24 @@ const form = document.querySelector(".login-form");
 form.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
-  event.preventDefault();
+  event.preventDefault(); //  обов'язково для того, щоб відмінити поведінку браузера за замовчуванням (в данному випадку (submit),
+  // щоб відмінити перезавантаження сторінки)
+
+  const elements = event.currentTarget.elements;
+  const email = elements.email.value.trim();
+  const password = elements.password.value.trim();
+
+  if (email === "" || password === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  const info = {
+    email: email,
+    password: password,
+  };
+
+  console.log(info);
+
+  event.currentTarget.reset();
 }
