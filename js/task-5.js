241e44731cb263af1b9dc6df7d7f3f7a000cf192
Nikel Stanislav <162141430+Nikel-Stanislav@users.noreply.github.com>
2024-07-01 19:22:49 +0300

    task-6

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const batton = document.querySelector(".change-color");
+const spanColor = document.querySelector(".color");
+
+batton.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  spanColor.textContent = randomColor;
+  console.log(spanColor);
+});
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,42 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controls = document.getElementById("controls");
+const input = controls.querySelector('input[type="number"]');
+const createButton = controls.querySelector("button[data-create]");
+const destroyButton = controls.querySelector("button[data-destroy]");
+const boxesContainer = document.getElementById("boxes");
+
+createButton.addEventListener("click", () => {
+  const amount = parseInt(input.value, 10);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = ""; // Очищення input після створення елементів
+  }
+});
+
+destroyButton.addEventListener("click", () => {
+  destroyBoxes();
+});
+
+function createBoxes(amount) {
+  destroyBoxes(); // Очищення попередніх елементів перед створенням нових
+  const fragment = document.createDocumentFragment();
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    fragment.appendChild(box);
+    size += 10;
+  }
+
+  boxesContainer.appendChild(fragment);
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
+}
